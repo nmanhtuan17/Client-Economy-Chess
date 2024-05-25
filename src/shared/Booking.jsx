a832667631f599311b7c879a711e794346043f1c
Huy2k3 <buohuy0201@gmail.com>
2024-05-25 10:42:17 +0700

    them discount

--- src/shared/Booking.jsx
+++ src/shared/Booking.jsx
@@ -17,6 +17,15 @@ const BookingPage = ({product, avgRating}) => {
   const { user } = useContext(AuthContext);
   const navigate = useNavigate();
 
+  // Kiểm tra nếu product không tồn tại hoặc không có giá trị
+  if (!product || !product.price) {
+    return null; // Trả về null nếu không có sản phẩm hoặc giá sản phẩm
+  }
+    // Tính toán giá sau khi giảm
+    const discountedPrice = product.discount ? Math.round(product.price - (product.price * product.discount / 100)) : product.price;
+    // Tính toán số tiền tiết kiệm được
+    const savings = product.discount ? Math.round(product.price - discountedPrice) : null;
+
   const handleQuantityChange = (value) => {
     setQuantity(value);
   };
@@ -60,14 +69,26 @@ const BookingPage = ({product, avgRating}) => {
           <span className="text-orange-500 mr-2">★★★★★</span>
           <span className="text-gray-600">{avgRating} reviews</span>
         </div>
-        <div className="text-3xl mb-4">{product.price}</div>
+        <div className="text-3xl mb-4">
+          
+          {product.discount && product.discount > 0 ? (
+                                  <div className="price-details">
+                                      <span className="discounted-price">${discountedPrice.toFixed(2)}  </span>
+                                      <span className="original-price">${product.price.toFixed(2)}  </span>
+                                  </div>
+                              ) : (
+                                  <h5>${product.price.toFixed(2)}  </h5>
+                              )}
+          {savings != null && <span className="savings">Save ${savings.toFixed(2)}</span>} {/* Hiển thị số tiền tiết kiệm */}
+        </div>
+        {/* <div className="text-3xl mb-4">${product.price}</div> */}
         <div className="flex items-center mb-4" style={{color:'#81B64C'}}>
-		<svg className="w-5 h-5 mr-2" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-			<path d="M0 9C0 4.02944 4.02944 0 9 0C13.9706 0 18 4.02944 18 9C18 13.9706 13.9706 18 9 18C4.02944 18 0 13.9706 0 9Z" fill="currentColor" />
-			<path d="M5 9.67188L7.44531 12L13 6.5" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-		</svg>
-		<span>In stock {product.stock_quantity}</span>
-		</div>
+        <svg className="w-5 h-5 mr-2" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M0 9C0 4.02944 4.02944 0 9 0C13.9706 0 18 4.02944 18 9C18 13.9706 13.9706 18 9 18C4.02944 18 0 13.9706 0 9Z" fill="currentColor" />
+          <path d="M5 9.67188L7.44531 12L13 6.5" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+        <span>In stock {product.stock_quantity}</span>
+        </div>
         <div className="mb-4">
 			<label htmlFor="quantity" className="block font-bold mb-2">
 				Quantity:
@@ -97,82 +118,9 @@ const BookingPage = ({product, avgRating}) => {
 				</button>
 			</div>
 			</div>
-        {/* <div className="bulk mb-4 ">
-          <label htmlFor="bulk-discounts" className="block font-bold mb-2">
-            Bulk Discounts:
-          </label>
-          <select
-            id="bulk-discounts"
-            className="w-full  hover:border-gray-500 px-4 py-2 pr-8 rounded  leading-tight focus:outline-none focus:shadow-outline"
-          >
-            <option value="2">2</option>
-            
-          </select>
-        </div> */}
-		{/* <div>
-			<p className='custome'><strong>CUSTOMIZE YOURS</strong></p>
-		</div>
-        <div className="board mb-4">
-          <label htmlFor="board" className="block font-bold mb-2">
-            Select A Board:
-          </label>
-          <select
-            id="board"
-            value={board}
-            onChange={(e) => handleBoardChange(e.target.value)}
-            className="w-full  border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded  leading-tight focus:outline-none focus:shadow-outline"
-          >
-            <option value="Green">Green</option>
-           
-          </select>
-        </div>
-        <div className="bag mb-4">
-          <label htmlFor="bag" className="block font-bold mb-2">
-            Select A Bag:
-          </label>
-          <select
-            id="bag"
-            value={bag}
-            onChange={(e) => handleBagChange(e.target.value)}
-            className="w-full border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded  leading-tight focus:outline-none focus:shadow-outline"
-          >
-			
-            <option value="Green">Green</option>
-            
-          </select>
-        </div>
-        <div className="mb-4">
-			<label htmlFor="pieces" className="block font-bold mb-2">
-				Select Pieces:
-			</label>
-			<div className="flex items-center">
-				<div className="mr-4">
-				<select
-					id="pieces"
-					value={pieces.black ? 'Black' : ''}
-					onChange={(e) => handlePiecesChange('black', e.target.value === 'Black')}
-					className="w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-				>
-					<option value="">None</option>
-					<option value="Black">Black</option>
-				</select>
-				</div>
-				<div>
-				<select
-					id="pieces"
-					value={pieces.white ? 'White' : ''}
-					onChange={(e) => handlePiecesChange('white', e.target.value === 'White')}
-					className="w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-				>
-					<option value="">None</option>
-					<option value="White">White</option>
-				</select>
-				</div>
-			</div>
-			</div> */}
         <button className="AddToCart text-white font-bold py-2 px-4 rounded flex items-center" onClick={handleAddToCart}>
           <span className="mr-2">ADD TO CART</span>
-          <span>| {product.price}</span>
+          <span>| ${product.price.toFixed(2)}</span>
         </button>
         {message && <div className="text-green-500 mt-4"> {message} </div>}
       </div>
--- src/shared/TourCard.jsx
+++ src/shared/TourCard.jsx
@@ -1,71 +1,53 @@
-
-
-import React from 'react'
-import {Card , CardBody} from 'reactstrap';
-import {Link} from 'react-router-dom';
+import React from 'react';
+import { Card, CardBody } from 'reactstrap';
+import { Link } from 'react-router-dom';
 import './tour-card.css';
 
-const TourCard = ({product}) => {
+const TourCard = ({ product }) => {
+    const { id, name, photo, price, discount, featured, stock_quantity } = product;
 
-  const {id, name, photo, price, featured, stock_quantity } = product;
-  // const totalRating = reviews?.reduce((acc, item) => acc + item.rating, 0);
-  
-  // const avgRating = totalRating === 0 ? "" : totalRating === 1 ? totalRating : totalRating / reviews?.length;
-  // let avgRating;
-  // if(totalRating === 0) {
-  //   avgRating = '';
-  // } else {
-  //   if(totalRating === 1) {
-  //     avgRating = totalRating;
-  //   } else {
-  //     avgRating = totalRating / (reviews?.length).toFixed(1);
+    // Tính toán giá sau khi giảm
+    const discountedPrice = discount ? Math.round(price - (price * discount / 100)) : price;
 
-  //   }
-  // }
-  //------ Calculator Rating -------------
+    // Tính toán số tiền tiết kiệm được
+    const savings = discount ? Math.round(price - discountedPrice) : null;
 
-  return (
-    <div className='tour__card'>
-      <Card>
-        <div className="tour__img">
-          <img src={photo} alt="tour-img" />
-          {featured === 1 ? <span>Featured</span> : null}
+    return (
+        <div className='tour__card'>
+            <Card>
+                <div className="tour__img">
+                    <img src={photo} alt="tour-img" />
+                    {featured === 1 ? <span>Featured</span> : null}
+                </div>
+                <CardBody>
+                    <div className="card__top d-flex items-center justify-between">
+                        {savings !== null && <span className="savings-text">Save ${savings.toFixed(2)}</span>}
+                    </div>
+
+                    <h5 className="tour__title">
+                        <Link to={`/products/${id}`}>{name}</Link>
+                    </h5>
+
+                    <div className="card__bottom mt-3 d-flex justify-between align-items-center font-bold text-sm">
+                        <div>
+                            {discount && discount > 0 ? (
+                                <div className="price-detail">
+                                    <span className="discounted-price">${discountedPrice.toFixed(2)} USD</span>
+                                    <span className="original-price">${price.toFixed(2)} USD</span>
+                                </div>
+                            ) : (
+                                <h5>${price.toFixed(2)} USD</h5>
+                            )}
+                        </div>
+                        
+                        <button className='btn booking__btn'>
+                            <Link to={`/products/${id}`}>View Product</Link>
+                        </button>
+                    </div>
+                </CardBody>
+            </Card>
         </div>
-        <CardBody>
-        <div className="card__top d-flex items-center justify-between">
-
-          {/* <span className="tour__rating d-flex items-center gap-1">
-            <i className="ri-star-fill "></i>
-            {avgRating === 0 ? null : avgRating}
-            {totalRating === 0 ? ( 'Not rated') 
-            : (
-                <span>({reviews.length})</span>
-              )
-            }
-            
-          </span> */}
-        </div>
-
-        <h5 className="tour__title">
-          <Link to={`/products/${id}`}>{name}</Link>
-        </h5>
-
-        <div className="card__bottom d-flex items-center justify-between mt-3">
-          <h5>${price} 
-            
-          </h5>
-
-          <button className='btn booking__btn'>
-            <Link to={`/products/${id}`}>View Product</Link>
-          </button>
-        </div>
-      </CardBody>
-
-      </Card>
-
-      
-    </div>
-  )
-}
+    );
+};
 
 export default TourCard;
